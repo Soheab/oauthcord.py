12b17e5b98dbc03bc695f79b5870b8022dbc9d11
Soheab <33902984+Soheab@users.noreply.github.com>
2026-04-27 17:49:31 +0200

    Remove unused imports

--- src/oauthcord/internals/endpoints/store.py
+++ src/oauthcord/internals/endpoints/store.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Mapping, Sequence
-from typing import TYPE_CHECKING, Any, cast
+from typing import TYPE_CHECKING
 
 import aiohttp
 
--- src/oauthcord/models/store.py
+++ src/oauthcord/models/store.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import datetime
-from typing import TYPE_CHECKING, Any, cast, override
+from typing import TYPE_CHECKING, Any, override
 
 from ..enums import (
     ContentRatingAgency,
